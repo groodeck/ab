91cefb73e2a610831b49b21c3c35e1a6916221a6
groodeck <grdck@c4550>
2014-11-04 22:58:00 +0100

    login with md5 support, subscribers page

--- src/main/java/org/ab/controller/SubscribersController.java
+++ src/main/java/org/ab/controller/SubscribersController.java
@@ -7,13 +7,12 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
-@RequestMapping("/login")
-public class LoginController {
-	private static final Logger logger = Logger.getLogger(LoginController.class.getName());
+@RequestMapping("/subscribers")
+public class SubscribersController {
+	private static final Logger logger = Logger.getLogger(SubscribersController.class.getName());
 	
 	@RequestMapping
-    public String showLoginPage(final Model model) {
-		return "login";
+    public String handleInitEntry(final Model model) {
+		return "subscribers";
     }
-	
 }
