54b2eb20ec24c32deb78f25743cc3d4c468ade2d
Dell <Dell@e4200>
2015-07-29 22:06:35 +0200

    Cities define screen - additional cleanup

--- src/main/java/org/ab/model/CitiesModel.java
+++ src/main/java/org/ab/model/CitiesModel.java
@@ -6,9 +6,6 @@ import org.assertj.core.util.Lists;
 
 public class CitiesModel {
 
-	private String newCityIdn;
-	private String newCityDesc;
-
 	private List<CityModel> cities;
 
 	public CitiesModel(){
@@ -19,24 +16,7 @@ public class CitiesModel {
 		return cities;
 	}
 
-	public String getNewCityDesc() {
-		return newCityDesc;
-	}
-
-	public String getNewCityIdn() {
-		return newCityIdn;
-	}
-
 	public void setCities(final List<CityModel> cities) {
 		this.cities = cities;
 	}
-
-	public void setNewCityDesc(final String newCityDesc) {
-		this.newCityDesc = newCityDesc;
-	}
-
-	public void setNewCityIdn(final String newCityIdn) {
-		this.newCityIdn = newCityIdn;
-	}
-
 }
